2da7c1cb349bddcfca4c38bdae4bd485c4ad5bc2
root <root@ip-172-31-4-20.ap-south-1.compute.internal>
2022-05-22 13:48:51 +0000

    maven

---
+++ src/main/java/com/kunalraut/app/App.java
@@ -0,0 +1,13 @@
+package com.kunalraut.app;
+
+/**
+ * Hello world!
+ * this is kunal raut hello to everyone
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
